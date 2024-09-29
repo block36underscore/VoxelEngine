f5904ef6f910b45a987124605ed329c50f7e67d6
block36_ <block36not33@gmail.com>
2024-09-29 19:13:18 -0400

    Create vulkan instance

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,19 @@
 package gay.block36.voxel
 
 import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWVulkan.glfwGetRequiredInstanceExtensions
+import org.lwjgl.system.MemoryStack
 import org.lwjgl.system.MemoryUtil.NULL
+import org.lwjgl.vulkan.VK10.*
+import org.lwjgl.vulkan.VkApplicationInfo
+import org.lwjgl.vulkan.VkInstance
+import org.lwjgl.vulkan.VkInstanceCreateInfo
 import kotlin.properties.Delegates
 
 
 fun main() {
     initWindow()
+    initVulkan()
 
     mainLoop@while (true) {
         if (glfwWindowShouldClose(Window)) break@mainLoop
@@ -15,6 +22,8 @@ fun main() {
 
         Thread.sleep(WindowInfo.LOOP_PERIOD)
     }
+
+    cleanup()
 }
 
 object WindowInfo {
@@ -25,6 +34,7 @@ object WindowInfo {
 }
 
 var Window: Long by Delegates.notNull()
+lateinit var Instance: VkInstance
 
 private fun initWindow() {
     if (!glfwInit()) {
@@ -47,10 +57,37 @@ private fun initWindow() {
 }
 
 private fun initVulkan() {
+    MemoryStack.stackPush().run {
+        val appInfo = VkApplicationInfo.calloc(this).apply {
+            sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
+            pApplicationName(this@run.UTF8Safe(WindowInfo.TITLE))
+            applicationVersion(VK_MAKE_VERSION(1, 0, 0))
+            pEngineName(this@run.UTF8Safe("No Engine"))
+            engineVersion(VK_MAKE_VERSION(1, 0, 0))
+            apiVersion(VK_API_VERSION_1_0)
+        }
 
+        val createInfo = VkInstanceCreateInfo.calloc(this).apply {
+            sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
+            pApplicationInfo(appInfo)
+            ppEnabledExtensionNames(glfwGetRequiredInstanceExtensions())
+            ppEnabledLayerNames(null)
+        }
+
+        val instancePtr = this.mallocPointer(1)
+
+        if (vkCreateInstance(createInfo, null, instancePtr) != VK_SUCCESS) {
+            throw RuntimeException("Failed to create vulkan instance")
+        }
+
+        Instance = VkInstance(instancePtr.get(0), createInfo)
+    }
 }
 
 private fun cleanup() {
+
+    vkDestroyInstance(Instance, null)
+
     glfwDestroyWindow(Window)
 
     glfwTerminate()
